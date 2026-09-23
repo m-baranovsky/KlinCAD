e878ed23b5ccb5cbc4634071f548829a9b9bcb53
m-baranovsky <stack.buffer.09@keemail.me>
2026-09-23 01:24:40 -0300

    Update app.py

--- src/klincad/app.py
+++ src/klincad/app.py
@@ -1,1 +1,16 @@
+import sys
 
+from PyQt5.QtWidgets import QApplication
+
+from klincad.editor.window import EditorCircuito
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = EditorCircuito()
+    ventana.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
